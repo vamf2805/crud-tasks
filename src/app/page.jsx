4ac856b9f56f7d3f588e6ac2aafd2832a44765a4
Victor Marquez <vamf2805@gmail.com>
2024-12-09 18:13:45 -0500

    Realizando responside design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ async function Home() {
   const tasks = await loadTasks()
   console.log(tasks)
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
       {
         tasks.map(task =>(
           <TaskCard
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,7 +6,7 @@ const TaskCard = ({ task }) => {
 
 	return (
 		<Link href={`/tasks/${_id}`}>
-			<div className="bg-gray-800 p-10 text-white rounded-md hover:cursor-pointer hover:bg-gray-700">
+			<div className="bg-gray-800 p-10 text-white rounded-md hover:cursor-pointer hover:bg-gray-700 h-full">
 				<h3 className="text-2xl font-bold ">{title}</h3>
 				<p className="text-slate-300">{description}</p>
 				<p className="text-slate-400 my-2">
